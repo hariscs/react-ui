acb2633715fa97a3e2fc45610a2c09fc180dfb40
Haris Shah <harisshah385@gmail.com>
2022-11-03 12:54:07 +0500

    Add Button Component

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,14 @@
-import 'react-app-polyfill/ie11';
+// import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { Button } from '../.';
 
 const App = () => {
   return (
     <div>
-      <Thing />
+      <Button variant="primary" onClick={() => console.log('clicked')}>
+        Click here
+      </Button>
     </div>
   );
 };
--- src/Button.tsx
+++ src/Button.tsx
@@ -1,25 +1,30 @@
 import React, { HTMLAttributes, ReactNode } from 'react';
 
+import styled from 'styled-components';
+
 import './tailwind.css';
 
+const PrimaryButton = styled.button.attrs(
+  ({ variant }: { variant: ButtonVariants }) => ({
+    className:
+      'px-8 py-2 font-semibold text-white transition duration-500 ease-in-out transform rounded-lg shadow-xl bg-gradient-to-r from-red-300 to-blue-300 hover:from-pink-400 hover:to-indigo-400',
+  })
+)<{ variant: ButtonVariants }>``;
+
+type ButtonVariants = 'default' | 'warning';
 export interface Props extends HTMLAttributes<HTMLButtonElement> {
   /** Button text here */
   children: ReactNode;
 
   /** there are two types of buttons */
-  variant: 'primary' | 'secondary';
+  variant?: ButtonVariants;
 }
 
 /** Main button component */
-export const Button = ({ children, variant = 'primary', ...props }: Props) => {
+export const Button = ({ children, variant, ...props }: Props) => {
   return (
-    <button
-      className={`${
-        variant === 'primary' ? 'bg-yellow-300' : 'bg-gray-300'
-      } text-white px-4 py-2  rounded-lg`}
-      {...props}
-    >
+    <PrimaryButton {...props} variant={variant}>
       {children}
-    </button>
+    </PrimaryButton>
   );
 };
