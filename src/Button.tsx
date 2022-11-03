70df20ce1c9c1096049ad6dce6eb30e91843a3a2
Haris Shah <harisshah385@gmail.com>
2022-11-03 16:49:56 +0500

    Add classnames pckg

--- src/Button.tsx
+++ src/Button.tsx
@@ -4,27 +4,23 @@ import styled from 'styled-components';
 
 import './tailwind.css';
 
-const PrimaryButton = styled.button.attrs(
-  ({ variant }: { variant: ButtonVariants }) => ({
-    className:
-      'px-8 py-2 font-semibold text-white transition duration-500 ease-in-out transform rounded-lg shadow-xl bg-gradient-to-r from-red-300 to-blue-300 hover:from-pink-400 hover:to-indigo-400',
-  })
-)<{ variant: ButtonVariants }>``;
-
-type ButtonVariants = 'default' | 'warning';
+const ButtonStyles = styled.button.attrs(({ variant }: { variant: Props }) => ({
+  className:
+    'px-8 py-2 font-semibold text-white transition duration-500 ease-in-out transform rounded-lg shadow-xl bg-gradient-to-r from-red-300 to-blue-300 hover:from-pink-400 hover:to-indigo-400',
+}))<{ variant: Props }>``;
 export interface Props extends HTMLAttributes<HTMLButtonElement> {
   /** Button text here */
   children: ReactNode;
 
   /** there are two types of buttons */
-  variant?: ButtonVariants;
+  variant?: 'default' | 'warning';
 }
 
 /** Main button component */
 export const Button = ({ children, variant, ...props }: Props) => {
   return (
-    <PrimaryButton {...props} variant={variant}>
+    <ButtonStyles {...props} variant={variant}>
       {children}
-    </PrimaryButton>
+    </ButtonStyles>
   );
 };
